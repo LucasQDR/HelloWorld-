6f4532ed34df702416ee05205e6e7e5b0ed8c264
Lucas Queiroz Rocha dos Reis <lucas_q_reis@estudante.sc.senai.br>
2021-11-29 17:57:10 -0200

    Contador

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ const display = document.getElementById("contador");
 
 let contar = 0;
 
-
-
 function somar(){
-    display.innerText = + 1
+    contar++
+    display.innerText = contar
 } 
 
+function subtrair(){
+    contar--
+    display.innerText = contar
+} 
+function reset(){
+    contar = 0;
+    display.innerText = contar 
+}
